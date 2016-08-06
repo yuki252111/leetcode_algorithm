95708a7a18277150f58390e90de8cb02d546ce98
Your Name <you@example.com>
2016-08-06 22:42:12 +0800

    2016-8-6

---
+++ leetcode/129_Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+	int sumNumbers(TreeNode* root) {
+		int sum = 0;
+		vector<int> onePath;
+		if (root) {
+			sumPath(root, sum, onePath);
+		}
+		return sum;
+	}
+private:
+	void sumPath(TreeNode* root, int& sum, vector<int>& onePath) {
+		if (root->left == NULL && root -> right == NULL) {
+			onePath.push_back(root->val);
+			int temp = 0;
+			int pathLen = onePath.size();
+			for (int i = 0; i < pathLen; i++) {
+				temp *= 10;
+				temp += onePath[i];
+			}
+			sum += temp;
+			onePath.pop_back();
+			return;
+		}
+		onePath.push_back(root->val);
+		if (root->left) {
+			sumPath(root->left, sum, onePath);
+		}
+		if (root->right) {
+			sumPath(root->right, sum, onePath);
+		}
+		onePath.pop_back();
+	}
+};
+
+int main() {
+	TreeNode* root = new TreeNode(0);
+	TreeNode* left = new TreeNode(1);
+	TreeNode* right = new TreeNode(3);
+	root->left = left;
+	root->right = right;
+	Solution s;
+	s.sumNumbers(root);
+}
---
+++ leetcode/leetcode128_Longest Consecutive Sequence.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int longestConsecutive(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		if (nums.size() <= 1) {
+			return nums.size();
+		}
+		int max = 0;
+		int temp = 1;
+		int size = nums.size();
+		for (int i = 1; i < size; i++) {
+			if (nums[i] - nums[i - 1] == 0) {
+				continue;
+			}
+			if (nums[i] - nums[i - 1] == 1) {
+				temp++;
+			}
+			else {
+				if (temp > max) {
+					max = temp;
+				}
+				temp = 1;
+			}
+		}
+		if (temp > max) {
+			max = temp;
+		}
+		return max;
+	}
+};
